fe084605c63e67a0dbb43853b49cbf03f4b4fdcc
neilmolky <neilmolky@zoho.com>
2022-03-21 18:03:22 +0000

    first commit

--- my_first_app.py
+++ my_first_app.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-msg = st.write("hello world")
+heading = st.header("A rather Cliche first app")
+
+with st.sidebar:
+    check = st.checkbox("change colours?")
+
+colour = "black"
+if check:
+    colour = "rainbow"
+
+
+msg = st.write("hello world", colour=colour)
+
+
+
